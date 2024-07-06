2407f39033fff7707eff4415420756de6c2e1197
Nadun Sanjeevana <108867149+NadunSanjeevana@users.noreply.github.com>
2024-07-06 21:27:42 +0530

    Meditation screen

--- constants/meditation-images.ts
+++ constants/meditation-images.ts
@@ -3,7 +3,7 @@ import meditatingUnderTree from "@/assets/meditation-images/meditate-under-tree.
 import riverImage from "@/assets/meditation-images/river.webp";
 import beachImage from "@/assets/meditation-images/beach.webp";
 import yosemiteStars from "@/assets/meditation-images/yosemite-stars.webp";
-import waterfall from "@asses/meditation-images/waterfall.webp";
+import waterfall from "@/assets/meditation-images/waterfall.webp";
 
 export default [
   treeImage,
